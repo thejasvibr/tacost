acf2fbb2bdf72e2039bfe4f85c35ed71f046d15a
tbeleyur <tbeleyur@orn.mpg.de>
2019-05-30 16:24:08 +0200

    version with bat flying away from array

--- making_sim_recordings.py
+++ making_sim_recordings.py
@@ -23,16 +23,17 @@ v_bat = 5.0 # m/s
 v_sound = 338.0 # m/s
 rec_durn = 4.0 # seconds
 t_emit = np.arange(0.1,rec_durn,0.1)
-traj_x = v_bat*t_emit - 6.0 # so that the bat makes a pass from the left to the right 
-traj_y = np.ones(traj_x.size)*2
+radius = 3.0
+traj_x = np.zeros(t_emit.size) # so that the bat makes a pass from the left to the right 
+traj_y = 1.5 + t_emit*v_bat
 traj_z = np.zeros(traj_x.size)
 trajectory = np.column_stack((traj_x, traj_y, traj_z)).reshape(-1,3)
 
 # define mic positions  and calculate the radial distance to the mics 
-mic_posns = np.array(([0,0,0],
-                      [-1,0,-1],
-                      [1,0,-1],
-                      [0,0,1]))
+mic_posns = np.array(([0,   0     ,0],
+                      [-0.52,-0.0,-0.3],
+                      [0.52,-0.0,-0.3],
+                      [0,-0.2,0.6]))
 
 # calculate mic-bat distances for each call;
 t_calls_at_mics = np.zeros((t_emit.size, mic_posns.shape[0]))
@@ -70,6 +71,7 @@ for each_channel in range(mic_posns.shape[0]):
     assign_call_to_mic(audio[:,each_channel], bat_call,
                                                  t_calls_at_mics[:,each_channel])
 
-sf.write('2d_WEIRDtristar_4channel_recording.WAV', audio, fs)
-#pd.DataFrame(data=np.column_stack((trajectory,t_emit)),
-#             columns=['x','y','z','t_emit']).to_csv('trajectory_path.csv')
+audio_file_name = 'bat_flying_in_arc_from_2D_60cmtristar'
+sf.write(audio_file_name+'.WAV', audio, fs)
+pd.DataFrame(data=np.column_stack((trajectory,t_emit)),
+             columns=['x','y','z','t_emit']).to_csv(audio_file_name+'_trajectory_path.csv')
